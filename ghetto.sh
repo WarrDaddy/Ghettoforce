615e7a53785f7ffdfde5f5bd1bb6fcae4a87b5c3
WarrDaddy <57737355+WarrDaddy@users.noreply.github.com>
2020-08-15 01:23:21 -0700

    Update ghetto.sh

--- ghetto.sh
+++ ghetto.sh
@@ -4,11 +4,15 @@ read -p "Please enter domain : " domain
 
 amass enum -passive -d $domain -o ~/Desktop/bounty/$domain.txt 
 
-input="/root/Desktop/bounty/$domain.txt"
+cat $domain.txt| httprobe >> validDomains.txt 
+
+input="/root/Desktop/bounty/validDomains.txt"
 while IFS= read -r line
 do
   domain="$line"
   echo "$domain"
-  /opt/ffuf/ffuf -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-small.txt -u http://line/FUZZ -v -o domain.json 
+  /opt/ffuf/ffuf -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-small.txt -u http://$line/FUZZ -mc 200 -o domain.json 
 
 done < "$input"
+
+
