38ed706b4263dd9ffd7ff54ed1f58085d0ef5868
WarrDaddy <57737355+WarrDaddy@users.noreply.github.com>
2020-08-20 22:37:43 -0700

    Update ghetto.sh

--- ghetto.sh
+++ ghetto.sh
@@ -1,18 +1,26 @@
-#!/bin/bash
+#/bin/bash 
+
+#DNS resolver refresh
+cd /opt/fresh.py && python3 /opt/fresh.py/fresh.py
+
+#Enter TLD
 echo 'syntax: example.com'
-read -p "Please enter domain : " domain
+read -p " Please enter enter domain : " domain 
 
-amass enum -passive -d $domain -o ~/Desktop/bounty/$domain.txt 
+#Amass subdomain discovery 
+amass enum -passive -d $domain -o ~/Desktop/bounty/$domain.amass
 
-cat $domain.txt| httprobe >> validDomains.txt 
+#massDNS 
+/opt/massdns/./bin/massdns -r /opt/fresh.py/resolvers.txt -t A -o S -w massDNSoutput.txt $domain.massDNS 
 
-input="/root/Desktop/bounty/validDomains.txt"
-while IFS= read -r line
-do
-  domain="$line"
-  echo "$domain"
-  /opt/ffuf/ffuf -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-small.txt -u http://$line/FUZZ -mc 200 -o domain.json 
+#parse file
+cat $domain.massDNS | awk '{print $1}' | sed 's/.$//' | sort -u > $domain.subdomains
 
+$bruteforce 
+input="~/root/Desktop/bounty/$domain.subdomains"
+while IFS= read -r line 
+do 
+   domain="$line"
+   echo "domain"
+   /opt/ffuf/ffuf -w /usr/share/wordlists/ez.txt -u http://$line/FUZZ -mc 200 -o domain.json
 done < "$input"
-
-
