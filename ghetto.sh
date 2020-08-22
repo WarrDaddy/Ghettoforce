8c8b7e037736844405e84b2a3f1dde558a4d561e
WarrDaddy <57737355+WarrDaddy@users.noreply.github.com>
2020-08-21 19:43:49 -0700

    Update ghetto.sh

--- ghetto.sh
+++ ghetto.sh
@@ -6,27 +6,21 @@
 #Enter TLD
 echo 'syntax: example.com'
 read -p "Please enter enter domain : " domain 
-mkdir $domain 
+mkdir $domain
 
 #Amass subdomain discovery 
-amass enum -passive -d $domain -o ~/Desktop/bounty/$domain/$domain.amass
+amass enum -passive -d $domain -o ~/Desktop/bounty/$domain/$domain.Amass
 
 #massDNS 
-/opt/massdns/./bin/massdns -r /opt/fresh.py/resolvers.txt -t A -o S -w ~/Desktop/bounty/$domain/$domain.massDNS ~/Desktop/bounty/$domain/$domain.amass 
+/opt/massdns/./bin/massdns -r /opt/fresh.py/resolvers.txt -t A -o S -w ~/Desktop/bounty/$domain/$domain.massDNS ~/Desktop/bounty/$domain/$domain.Amass 
 
 #parse file
-cat ~/Desktop/bounty/$domain/$domain.massDNS | awk '{print $1}' | sed 's/.$//' | sort -u > ~/Desktop/bounty/$domain/$domain.subdomains
+cat ~/Desktop/bounty/$domain/$domain.massDNS | awk '{print $1}' | sed 's/.$//' | sort -u > ~/Desktop/bounty/$domain/$domain.Subdomain
 
-
-#bruteforce
-input=~/Desktop/bounty/$domain/$domain.subdomains
-cat $input
+input="$~/Desktop/bounty/$domain/$domain.Subdomain"
 while IFS= read -r line
 do
   domain="$line"
   echo "$domain"
-  /opt/ffuf/ffuf -w /usr/share/wordlists/Content.txt -u http://$line/FUZZ -mc 200 -o ~/Desktop/bounty/$domain/domain.json
-
-done < "$input"
-
-
+  /opt/ffuf/ffuf -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-small.txt -u http://line/FUZZ -mc 200 -o ~/Desktop/bounty/$domain/domain.json
+done 
